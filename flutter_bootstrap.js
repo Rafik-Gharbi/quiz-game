ec12a4cffe0e9b8af874ee883f956a6c4eac5b77
Rafik-Gharbi <Rafik-Gharbi@users.noreply.github.com>
2025-12-10 08:06:00 +0000

    deploy: de5a97b138194f178bf922816c04bbad1c27b1f0

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d3d45dcf251823c1769909cd43698d126db38d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3574048741"
+    serviceWorkerVersion: "3871480769"
   }
 });
